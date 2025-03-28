62aabefd27e23e5939645a7280e4fae0982e205c
Elliott Rahming <63752055+Ely242@users.noreply.github.com>
2025-03-27 20:01:26 -0400

    forgot to decrement 'components'

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -8,14 +8,14 @@ private:
     vector<int> parent, rank;
     // vector<int> size;
     int components;
+
 public:
     UnionFind(int n) {
         parent.resize(n);
-        rank.resize(n);
+        rank.resize(n, 0); // Initialize rank to 0
         components = n;
         for (int i = 0; i < n; i++) {
             parent[i] = i;
-            rank[i] = 1;
             // size[i] = 1;
         }
     }
@@ -27,7 +27,7 @@ public:
      */
     int find(int x) {
         if (parent[x] != x) {
-            parent[x] = find(parent[x]);
+            parent[x] = find(parent[x]); // Path compression
         }
         return parent[x];
     }
@@ -41,22 +41,23 @@ public:
     bool merge(int x, int y) {
         int rootX = find(x);
         int rootY = find(y);
-        if (rootX != rootY) {
-            if (rank[rootX] > rank[rootY]){
-                parent[rootY] = rootX;
-                // size[rootX] += size[rootY];
-            }
-            else if (rank[rootX] < rank[rootY]){
-                parent[rootX] = rootY;
-                // size[rootY] += size[rootX];
-            }
-            else {
-                parent[rootY] = rootX;
-                rank[rootX]++;
-                // size[rootX] += size[rootY];
-            }
+
+        if (rootX == rootY) return false; // Already in the same set
+
+        if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+            // size[rootX] += size[rootY];
+        } else if (rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+            // size[rootY] += size[rootX];
+        } else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
+            // size[rootX] += size[rootY];
         }
-        return false;
+
+        components--; // Decrease the number of connected components
+        return true;
     }
 
     /**
@@ -68,15 +69,6 @@ public:
     bool connected(int x, int y) {
         return find(x) == find(y);
     }
-
-    /**
-     *  Get the size of the set in which element x belongs.
-     * @param x The element to find.
-     * @return The size of the set in which element x belongs.
-     */
-    // int getSize(int x) {
-    //     return size[find(x)];
-    // }
 
     /**
      * Get the number of connected components.
